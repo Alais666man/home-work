2e4083b25d9604791b15388ca726f51a137b4d94
Alais666man <alais666man@gmail.com>
2018-04-18 14:58:42 +0300

    Треугольник1

--- task_triangle_ya.py
+++ task_triangle_ya.py
@@ -10,12 +10,12 @@ print('Точка C [x y]')
 Cx = int(input())
 Cy = int(input())
 print(Cx,Cy)
-AB = (Ax - Bx)**2 + (Ay - By)**2
-BC = (Bx - Cx)**2 + (By - Cy)**2
-AC = (Ax - Cx)**2 + (Ay - Cy)**2
+AB = ((Ax - Bx)**2 + (Ay - By)**2)**0.5
+BC = ((Bx - Cx)**2 + (By - Cy)**2)**0.5
+AC = ((Ax - Cx)**2 + (Ay - Cy)**2)**0.5
 print(AB, BC, AC)
 for i in range(1):
-    if AB**2 == BC**2 +AC**2 or BC**2 == AC**2 + AB**2 or AC**2 == AB**2 + BC**2:
+    if AB == (BC**2 +AC**2)**0.5 or BC == (AC**2 + AB**2)**0.5 or AC == (AB**2 + BC**2)**0.5:
         print('yes')
     else:
         print('no')
