4d1f211a35139ab0061d752b4b3c5c512981c429
Alais666man <alais666man@gmail.com>
2018-04-19 02:59:01 +0300

    Try while

---
+++ task_bubble_sort.py
@@ -0,0 +1,12 @@
+lst = [14, 8, 3, 1, 89, 2, 45]
+def bubble_sort(lst):
+    while n < len(lst):
+        for i=1 in range(0, (lst) - n):
+            if lst[i] > lst[i + 1]:
+                lst[i], lst[i + 1] == lst[i]
+                
+            
+    
+    for i=1 in n - 1 range(0):
+        
+        
