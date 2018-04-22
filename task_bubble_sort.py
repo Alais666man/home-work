8df6650f5ae2f94a4b8067bf20303eca1163fbbb
Alais666man <alais666man@gmail.com>
2018-04-22 23:51:13 +0300

    Не работает

--- task_bubble_sort.py
+++ task_bubble_sort.py
@@ -1,20 +1,57 @@
-
-
-def bubble_sort(spisok):
-    n = 1
-    while n < len(spisok):
-        
-        for i in range(len(spisok) - n):
-            if spisok[i] > spisok[i + 1]:
-                spisok[i], spisok[i + 1] = spisok[i + 1], spisok[i]
-                n = n + 1
-                
-spisok = [14, 8, 3, 1, 89, 2, 45]            
-bubble_sort(spisok) 
-print(spisok)
-                
+def bubble_sort(lst):
+    for j in range (len(lst), 0, -1):
+        for i in range(0, len(lst) - 1):
+            if lst[i] > lst[i + 1]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+                return lst
             
-    
+                
 
-        
-        
+
+            
+  #a = [1, 8, 3, 5, 7, 4]
+
+#print(a)
+
+#for i in range(len(a), 0, -1): 
+    #for k in range(0, len(a) - 1):
+        #if a[k] > a[k + 1]:
+            #a[k], a[k + 1] = a[k +1], a[k]
+#print(a)
+
+
+
+#def bubble_sort(lst):
+    #n=len(a)
+    #m=n-1
+    #while m>0:
+        #for i in range(m):
+            #if (a[i]>a[i+1]):
+                #x=a[i]
+                #a[i]=a[i+1]
+                #a[i+1]=x
+                #m=m-1
+                #return lst
+                      
+#a=[1,0,9]
+#print (a)
+#n=len(a)
+#m=n-1
+#while m>0:
+    #for i in range(m):
+        #if (a[i]>a[i+1]):
+            #x=a[i]
+            #a[i]=a[i+1]
+            #a[i+1]=x
+            #m=m-1
+#print(a)            
+
+
+
+
+
+
+
+
+
+
