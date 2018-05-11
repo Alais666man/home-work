e5192d14cf37d9a1146383e460958acab74073f8
Alais666man <alais666man@gmail.com>
2018-05-11 12:19:45 +0300

    Закончен

--- Ezidnevnik/consol-ezi/consol_ezi/__init__.py
+++ Ezidnevnik/consol-ezi/consol_ezi/__init__.py
@@ -49,8 +49,8 @@ def action_add():
     task_status = 'В процессе'
     with get_connection() as conn:
         name = storage.add_name(conn, task_name, task_description, task_status)
+    action_find_all()
     main()
-
 
 
 def action_find_all():
@@ -58,16 +58,18 @@ def action_find_all():
     with get_connection() as conn:
         tasks = storage.find_all(conn)
 
-    # template = '{task[id]} - {task[task_name]} - {task[task_description]} - {task[task_status]} - {task[created]}'
-    #
-    # print(template.format(task=tasks))
+        template = '{task[id]}/ {task[task_name]} => {task[task_description]} | {task[task_status]} | {task[created]} |'
+        for task in tasks:
+            print(template.format(task=task))
     main()
+
 
 def action_del():
     id = input('\nВведите id задачи для удаления: ')
     with get_connection() as conn:
         delete = storage.delete_task(conn, id)
-        # action_find_all()
+    action_find_all()
+    main()
 
 
 def action_status_end():
@@ -75,6 +77,8 @@ def action_status_end():
     # task_status = 'Завершена'
     with get_connection() as conn:
         status = storage.change_status_end(conn, id)
+    action_find_all()
+    main()
 
 
 def action_status_begin():
@@ -82,6 +86,8 @@ def action_status_begin():
     # task_status = 'Завершена'
     with get_connection() as conn:
         status = storage.change_status_begin(conn, id)
+    action_find_all()
+    main()
 
 
 def action_change():
@@ -89,7 +95,9 @@ def action_change():
     task_name = input('\nВведите имя задачи: ')
     task_description = input('\nВведите текст задачи: ')
     with get_connection() as conn:
-        task = storage.change_task(conn, id, task_name, task_description)
+        task = storage.change_task(conn, task_name, task_description, id)
+    action_find_all()
+    main()
 
 
 def action_exit():
--- Ezidnevnik/consol-ezi/consol_ezi/storage.py
+++ Ezidnevnik/consol-ezi/consol_ezi/storage.py
@@ -2,22 +2,25 @@ import os.path as Path
 import sqlite3
 
 SQL_ADD_NAME = 'INSERT INTO task (task_name, task_description, task_status) VALUES(?,?,?)'
-# SQL_ADD_DESCRIPTION = 'INSERT INTO task (task_description) VALUES (?)'
-# SQL_ADD_STATUS = 'INSERT INTO task (task_status) VALUES (?)'
-
-
 SQL_SELECT_ALL = '''
     SELECT
         id, task_name, task_description, task_status, created
     FROM
         task    
 '''
-
 SQL_FIND_ALL = SQL_SELECT_ALL + ' WHERE id=?'
 SQL_DEL_TASK = 'DELETE FROM task WHERE id=?'
 SQL_UPDATE_STATUS_END = 'UPDATE task SET task_status="Завершена" WHERE id=?'
 SQL_UPDATE_STATUS_BEGIN = 'UPDATE task SET task_status="В процессе" WHERE id=?'
 SQL_UPDATE_TASK = 'UPDATE task SET task_name=?, task_description=?, task_status ="В процессе" WHERE id=?'
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 
 def connect(db_name=None):
     """Подкдючение к БД"""
@@ -25,6 +28,7 @@ def connect(db_name=None):
         db_name = ':memory:'
 
     conn = sqlite3.connect(db_name)
+    conn.row_factory = dict_factory
     return conn
 
 
@@ -37,45 +41,35 @@ def initialize(conn):
 
 
 def add_name(conn, task_name, task_description, task_status):
-    cursor = conn.execute(SQL_ADD_NAME, (task_name, task_description, task_status))
+    cursor = conn.execute(SQL_ADD_NAME, (task_name, task_description, task_status,))
     return task_name, task_description, task_status
-
-
-# def add_description(conn, task_description):
-#     cursor = conn.execute(SQL_ADD_DESCRIPTION, (task_description,))
-#     return task_description
-#
-#
-# def add_status(conn, task_status):
-#     cursor = conn.execute(SQL_ADD_STATUS, (task_status,))
-#     return task_status
 
 
 def find_all(conn):
     """Выводит все задачи"""
     with conn:
         cursor = conn.execute(SQL_SELECT_ALL)
-        return print(cursor.fetchall())
+        return cursor.fetchall()
 
 
 def delete_task(conn, id):
-    cursor = conn.execute(SQL_DEL_TASK, (id))
+    cursor = conn.execute(SQL_DEL_TASK, (id,))
 
 
 def change_status_end(conn, id):
     with conn:
-        cursor = conn.execute(SQL_UPDATE_STATUS_END, (id))
+        cursor = conn.execute(SQL_UPDATE_STATUS_END, (id,))
 
 
 def change_status_begin(conn, id):
     with conn:
-        cursor = conn.execute(SQL_UPDATE_STATUS_BEGIN, (id))
+        cursor = conn.execute(SQL_UPDATE_STATUS_BEGIN, (id,))
 
 
-def change_task(conn, id, task_name, task_description):
+def change_task(conn, task_name, task_description, id):
     with conn:
-        cursor = conn.execute(SQL_UPDATE_TASK, (id, task_name, task_description))
-    return id, task_name, task_description
+        cursor = conn.execute(SQL_UPDATE_TASK, (task_name, task_description, id,))
+    return task_name, task_description
 
 
 
