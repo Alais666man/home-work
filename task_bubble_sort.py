ccb68f60711bd40e44c89137a6558b2afec7e0d9
Alais666man <alais666man@gmail.com>
2018-05-14 11:21:03 +0300

    Сортировка

--- task_bubble_sort.py
+++ task_bubble_sort.py
@@ -1,57 +1,7 @@
 def bubble_sort(lst):
-    for j in range (len(lst), 0, -1):
-        for i in range(0, len(lst) - 1):
-            if lst[i] > lst[i + 1]:
-                lst[i], lst[i + 1] = lst[i + 1], lst[i]
-                return lst
-            
-                
-
-
-            
-  #a = [1, 8, 3, 5, 7, 4]
-
-#print(a)
-
-#for i in range(len(a), 0, -1): 
-    #for k in range(0, len(a) - 1):
-        #if a[k] > a[k + 1]:
-            #a[k], a[k + 1] = a[k +1], a[k]
-#print(a)
-
-
-
-#def bubble_sort(lst):
-    #n=len(a)
-    #m=n-1
-    #while m>0:
-        #for i in range(m):
-            #if (a[i]>a[i+1]):
-                #x=a[i]
-                #a[i]=a[i+1]
-                #a[i+1]=x
-                #m=m-1
-                #return lst
-                      
-#a=[1,0,9]
-#print (a)
-#n=len(a)
-#m=n-1
-#while m>0:
-    #for i in range(m):
-        #if (a[i]>a[i+1]):
-            #x=a[i]
-            #a[i]=a[i+1]
-            #a[i+1]=x
-            #m=m-1
-#print(a)            
-
-
-
-
-
-
-
-
-
+    for j in range(len(lst) - 1):                               # Проход по каждлму элементу списка
+        for i in range(len(lst) - 1):                           # Проход по элемнту в паре
+            if lst[i] > lst[i + 1]:                             # Если первый элемент пары больше второго
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]         # Меняем элементы пары местами
+    return lst
 
