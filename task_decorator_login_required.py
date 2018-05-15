0a5329b252e84d83bb362d2794633c33ce28d1af
Alais666man <alais666man@gmail.com>
2018-05-15 17:35:06 +0300

    Пока не работает

---
+++ task_decorator_login_required.py
@@ -0,0 +1,36 @@
+from functools import lru_cache
+import hashlib
+
+
+def make_token(username, password):
+    with open('token.txt', 'w') as f:
+        s = username + password
+        return f.write(hashlib.md5(s.encode()).hexdigest())
+
+
+make_token(username=input(), password=input())
+
+
+@lru_cache(maxsize=None)
+def login_required(username, password):
+    def decorator(func):
+
+        def wrapper(*args, **kwargs):
+            with open('token.txt') as f:
+                hash = f.read()
+                result = func(*args, **kwargs)
+                t = 0
+                while t < 3:
+                    username = input()
+                    password = input()
+                    s = username + password
+                    if hash != hashlib.md5(s.encode()).hexdigest():
+                        print('Функция защищена паролем')
+                        t += 1
+                    else:
+                        return result
+        return wrapper
+
+
+login_required()
+
