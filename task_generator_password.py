39856d11cd9055679e9535f036a85edcad97a54d
Alais666man <alais666man@gmail.com>
2018-05-20 23:04:14 +0300

    Генератор паролей

--- task_generator_password.py
+++ task_generator_password.py
@@ -1,10 +1,11 @@
-import random, string
+import random
+import string
 
 
 def password_generator(n):
     while True:
-        parol = ''
-        yield parol.join(random.sample(string.ascii_uppercase + string.ascii_lowercase
+
+        yield ''.join(random.sample(string.ascii_uppercase + string.ascii_lowercase
                          + string.digits + string.punctuation, n))
 
 
